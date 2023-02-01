9a1c03ff8b7a9c66948b9bc811dde63b6cd0f77e
triciahughes <hughes.a.tricia@gmail.com>
2023-02-01 10:25:36 -0800

    second deliverables met

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false);
+
+  function handleCartClick() {
+    setIsInCart(!isInCart);
+  }
+
+  const classNameText = isInCart ? "in-cart" : "";
+  const cartButtonText = isInCart ? "Remove From Cart" : "Add To Cart";
+  const cartButtonClass = isInCart ? "remove" : "add";
+
   return (
-    <li className="">
+    <li className={classNameText}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={cartButtonClass} onClick={handleCartClick}>
+        {cartButtonText}
+      </button>
     </li>
   );
 }
